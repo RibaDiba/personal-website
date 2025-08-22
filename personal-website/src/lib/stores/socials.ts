187e9f892a0fba9bf8188e3ea875c2e2b8bccaa7
RibaDiba <abrmod74@gmail.com>
2025-08-21 22:23:52 -0400

    finished desktop

---
+++ personal-website/src/lib/stores/socials.ts
@@ -0,0 +1,23 @@
+import im_insta from "../assets/logos_socials/Instagram.png"
+import im_linkedin from "../assets/logos_socials/Linkedin.png"
+import im_github from "../assets/logos_socials/Github.png"
+
+
+export const socials = [
+    {
+        name: "Github",
+        link: "https://github.com/RibaDiba",
+        image: im_github
+    },
+    {
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/abir-modak-b7151328b/",
+        image: im_linkedin
+    },
+    {
+        name: "Instagram",
+        link: "https://www.instagram.com/ribadiba_/",
+        image: im_insta
+    },
+]
+
