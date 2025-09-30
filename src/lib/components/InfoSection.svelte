acd8844db0182e0db1e92991dc6f35cf29bea398
RibaDiba <abrmod74@gmail.com>
2025-09-30 18:39:39 -0400

    fux

--- src/lib/components/InfoSection.svelte
+++ src/lib/components/InfoSection.svelte
@@ -3,6 +3,7 @@
 	import AboutMe from "./AboutMe.svelte";
 	import Skills from "./Skills.svelte";
 	import Projects from "./Projects.svelte";
+    import { base } from "$app/paths";
 </script>
 
 <main class="flex flex-col pt-10 lg:pt-20 justify-center items-center px-4 lg:px-10">
@@ -11,7 +12,7 @@
     <div class="flex justify-center items-center relative mb-8 lg:mb-12 mt-15 md:mt-0">
         <div class="z-0 absolute bg-base-300 rounded-full w-[100px] h-[100px] lg:w-[150px] lg:h-[150px] left-[100px] lg:left-[150px] top-[-15px] lg:top-[-20px]"></div>
         <div class="rounded-full w-[180px] h-[180px] lg:w-[250px] lg:h-[250px] bg-[#D9D9D9] relative p-3">
-            <img src="/Profile.jpg" alt="" class="w-full h-full object-cover rounded-full">
+            <img src="{base}/Profile.jpg" alt="" class="w-full h-full object-cover rounded-full">
         </div>
     </div>
 
